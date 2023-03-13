8e17f806d88371097db404a24ff3c5f5ec072ec5
lzhagjm <lzh-pc@outlook.com>
2023-03-13 10:44:15 +0800

    add new file

---
+++ Core/Src/freertos.c
@@ -0,0 +1,270 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * File Name          : freertos.c
+  * Description        : Code for freertos applications
+  ******************************************************************************
+  * @attention
+  *
+  * Copyright (c) 2023 STMicroelectronics.
+  * All rights reserved.
+  *
+  * This software is licensed under terms that can be found in the LICENSE file
+  * in the root directory of this software component.
+  * If no LICENSE file comes with this software, it is provided AS-IS.
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+
+/* Includes ------------------------------------------------------------------*/
+#include "FreeRTOS.h"
+#include "task.h"
+#include "main.h"
+#include "cmsis_os.h"
+
+/* Private includes ----------------------------------------------------------*/
+/* USER CODE BEGIN Includes */
+#include "fatfs.h"
+#include "sdio.h"
+#include <string.h>
+#include "dma2d.h"
+#include <stdio.h>
+/* USER CODE END Includes */
+
+/* Private typedef -----------------------------------------------------------*/
+/* USER CODE BEGIN PTD */
+
+/* USER CODE END PTD */
+
+/* Private define ------------------------------------------------------------*/
+/* USER CODE BEGIN PD */
+#define IMGRAM_BASE_ADDR 0xD0000000
+
+
+/* USER CODE END PD */
+
+/* Private macro -------------------------------------------------------------*/
+/* USER CODE BEGIN PM */
+
+/* USER CODE END PM */
+
+/* Private variables ---------------------------------------------------------*/
+/* USER CODE BEGIN Variables */
+
+/************Fatfs Task****************/
+osThreadId_t FatfsTaskHandle;
+const osThreadAttr_t FatfsTask_attributes = {
+  .name = "fatfsTask",
+  .stack_size = 1024 * 4,
+  .priority = (osPriority_t) osPriorityNormal,
+};
+
+/************LCD Task****************/
+osThreadId_t LcdTaskHandle;
+const osThreadAttr_t LcdTask_attributes = {
+  .name = "lcdTask",
+  .stack_size = 512 * 4,
+  .priority = (osPriority_t) osPriorityNormal,
+};
+
+
+/* USER CODE END Variables */
+/* Definitions for defaultTask */
+osThreadId_t defaultTaskHandle;
+const osThreadAttr_t defaultTask_attributes = {
+  .name = "defaultTask",
+  .stack_size = 128 * 4,
+  .priority = (osPriority_t) osPriorityNormal,
+};
+
+/* Private function prototypes -----------------------------------------------*/
+/* USER CODE BEGIN FunctionPrototypes */
+void FatfsTask(void *argument);
+void LcdTask(void *argument);
+//static void draw_xy_wh_c(uint16_t x, uint16_t y, uint16_t w, uint16_t h, uint32_t c);
+
+/* USER CODE END FunctionPrototypes */
+
+void StartDefaultTask(void *argument);
+
+extern void MX_LWIP_Init(void);
+void MX_FREERTOS_Init(void); /* (MISRA C 2004 rule 8.1) */
+
+/**
+  * @brief  FreeRTOS initialization
+  * @param  None
+  * @retval None
+  */
+void MX_FREERTOS_Init(void) {
+  /* USER CODE BEGIN Init */
+
+  /* USER CODE END Init */
+
+  /* USER CODE BEGIN RTOS_MUTEX */
+  /* add mutexes, ... */
+  /* USER CODE END RTOS_MUTEX */
+
+  /* USER CODE BEGIN RTOS_SEMAPHORES */
+  /* add semaphores, ... */
+  /* USER CODE END RTOS_SEMAPHORES */
+
+  /* USER CODE BEGIN RTOS_TIMERS */
+  /* start timers, add new ones, ... */
+  /* USER CODE END RTOS_TIMERS */
+
+  /* USER CODE BEGIN RTOS_QUEUES */
+  /* add queues, ... */
+  /* USER CODE END RTOS_QUEUES */
+
+  /* Create the thread(s) */
+  /* creation of defaultTask */
+  defaultTaskHandle = osThreadNew(StartDefaultTask, NULL, &defaultTask_attributes);
+
+  /* USER CODE BEGIN RTOS_THREADS */
+  /* add threads, ... */
+  /* USER CODE END RTOS_THREADS */
+
+  /* USER CODE BEGIN RTOS_EVENTS */
+  /* add events, ... */
+  /* USER CODE END RTOS_EVENTS */
+
+}
+
+/* USER CODE BEGIN Header_StartDefaultTask */
+/**
+  * @brief  Function implementing the defaultTask thread.
+  * @param  argument: Not used
+  * @retval None
+  */
+/* USER CODE END Header_StartDefaultTask */
+void StartDefaultTask(void *argument)
+{
+  /* init code for LWIP */
+  MX_LWIP_Init();
+  /* USER CODE BEGIN StartDefaultTask */
+
+	FatfsTaskHandle = osThreadNew(FatfsTask, NULL, &FatfsTask_attributes);
+	LcdTaskHandle = osThreadNew(LcdTask, NULL, &LcdTask_attributes);
+
+    for(;;)
+    {
+    	HAL_GPIO_TogglePin(GPIOH, GPIO_PIN_10);
+      osDelay(1000);
+    }
+  /* USER CODE END StartDefaultTask */
+}
+
+/* Private application code --------------------------------------------------*/
+/* USER CODE BEGIN Application */
+
+static void draw_xy_wh_c(uint16_t x, uint16_t y, uint16_t w, uint16_t h, uint32_t c){
+	dma2d_put_src_wh_c(IMGRAM_BASE_ADDR + y * 1024 * 3 + x * 3, w, h, c);
+}
+
+void FatfsTask(void *argument){
+	int ret = 0;
+	char path[50];
+	const char *write_buff = "hello world! xsdhjkshfksehifuhseuhfuisdhfuise";
+	char readbuff[100];
+	HAL_SD_CardInfoTypeDef pCardInfo;
+	FIL SDFile1, SDFile2;
+	UINT bw = 0, br = 0;
+	/*1、挂载文件系统*/
+	BYTE work[_MAX_SS];
+	ret = f_mount (&SDFatFS, (const TCHAR*)SDPath, 1);
+	if(ret == FR_NO_FILESYSTEM){
+		ret = f_mkfs ((const TCHAR*)SDPath, FM_FAT32, 0, work, sizeof(work));
+		if(ret != FR_OK){
+				printf("f_mkfs is err:%d\r\n",ret);
+				goto loop;
+		}
+		f_mount (&SDFatFS, SDPath, 1);
+	}else if (ret != FR_OK){
+			printf("f_mount is err:%d\r\n",ret);
+			goto loop;
+	}else{
+		printf("f_mount is ok\r\n");
+	}
+	ret = HAL_SD_GetCardInfo(&hsd, &pCardInfo);
+	if(ret != HAL_OK){
+		printf("HAL_SD_GetCardInfo is err\r\n");
+	}else{
+		printf("Initialize SD card successfully!\r\n");
+		 // 打印SD卡基本信�????????????
+		 printf(" SD card information! \r\n");
+		 printf(" CardBlockSize : %ld \r\n", pCardInfo.BlockSize);   // 块大�????????????
+		 printf(" CardBlockNbr : %ld \r\n", pCardInfo.BlockNbr);   // 块大�????????????
+		 printf(" CardCapacity  : %.2f MB \r\n",(double)((unsigned long long)pCardInfo.BlockSize * pCardInfo.BlockNbr/1024)/1024);// 显示容量 // @suppress("Float formatting support")
+		 printf(" LogBlockNbr   : %ld \r\n", pCardInfo.LogBlockNbr); // 逻辑块数�????????????
+		 printf(" LogBlockSize  : %ld \r\n", pCardInfo.LogBlockSize);// 逻辑块大�????????????
+		 printf(" RCA           : %ld \r\n", pCardInfo.RelCardAdd);  // 卡相对地�????????????
+		 printf(" CardType      : %ld \r\n", pCardInfo.CardType);    // 卡类�????????????
+		 // 读取并打印SD卡的CID信息
+		 HAL_SD_CardCIDTypeDef sdcard_cid;
+		 HAL_SD_GetCardCID(&hsd,&sdcard_cid);
+		 printf(" ManufacturerID: %d \r\n",sdcard_cid.ManufacturerID);
+	}
+		/*2、新建test.txt文件*/
+		sprintf(path, "%stest.txt",SDPath);
+		ret = f_open (&SDFile1, path, FA_READ | FA_WRITE);
+		if(ret != FR_OK){
+			printf("f_open is err:%d\r\n",ret);
+			goto f_open_err;
+		}
+		/*将缓存写入文�?????????????????*/
+
+		ret = f_write (&SDFile1, write_buff, strlen(write_buff), &bw);
+		if(ret != FR_OK){
+				printf("f_write is err:%d\r\n",ret);
+				goto f_write_err;
+		}
+		printf("write is ok: bw:%d\r\n",bw);
+		/*读取文件 0:/xixi.txt*/
+		sprintf(path, "%sxixi.txt",SDPath);
+		ret = f_open (&SDFile2, path, FA_READ);
+		if(ret != FR_OK){
+			printf("f_open is err:%d\r\n",ret);
+		}
+		ret = f_read (&SDFile2, readbuff, 16, &br);
+		if(ret != FR_OK){
+				printf("f_read is err:%d\r\n",ret);
+		}
+		readbuff[16] = 0;
+		printf("%s\r\n",readbuff);
+		f_close (&SDFile1);
+		f_close (&SDFile2);
+		f_mount(NULL, SDPath, 1);
+		goto loop;
+		/* Infinite loop */
+	f_write_err:
+		f_close (&SDFile1);
+	f_open_err:
+		f_mount(NULL, SDPath, 1);
+		loop:
+	while(1){
+		osDelay(1000);
+	};
+}
+
+
+void LcdTask(void *argument){
+
+	while(1){
+		draw_xy_wh_c(0, 0, 1024, 600, 0);
+		osDelay(1000);
+		draw_xy_wh_c(0, 0, 1024, 600, 0xFF0000);
+		osDelay(1000);
+		draw_xy_wh_c(0, 0, 1024, 600, 0x00FF00);
+		osDelay(1000);
+		draw_xy_wh_c(0, 0, 1024, 600, 0x0000FF);
+		osDelay(1000);
+		draw_xy_wh_c(0, 0, 1024, 600, 0xFFFFFF);
+		osDelay(1000);
+	}
+}
+
+
+
+/* USER CODE END Application */
+
---
+++ Debug/Core/Src/subdir.mk
@@ -0,0 +1,72 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+# Toolchain: GNU Tools for STM32 (10.3-2021.10)
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../Core/Src/crc.c \
+../Core/Src/dma.c \
+../Core/Src/dma2d.c \
+../Core/Src/fmc.c \
+../Core/Src/freertos.c \
+../Core/Src/gpio.c \
+../Core/Src/ltdc.c \
+../Core/Src/main.c \
+../Core/Src/rng.c \
+../Core/Src/sdio.c \
+../Core/Src/stm32f4xx_hal_msp.c \
+../Core/Src/stm32f4xx_it.c \
+../Core/Src/syscalls.c \
+../Core/Src/sysmem.c \
+../Core/Src/system_stm32f4xx.c \
+../Core/Src/usart.c 
+
+OBJS += \
+./Core/Src/crc.o \
+./Core/Src/dma.o \
+./Core/Src/dma2d.o \
+./Core/Src/fmc.o \
+./Core/Src/freertos.o \
+./Core/Src/gpio.o \
+./Core/Src/ltdc.o \
+./Core/Src/main.o \
+./Core/Src/rng.o \
+./Core/Src/sdio.o \
+./Core/Src/stm32f4xx_hal_msp.o \
+./Core/Src/stm32f4xx_it.o \
+./Core/Src/syscalls.o \
+./Core/Src/sysmem.o \
+./Core/Src/system_stm32f4xx.o \
+./Core/Src/usart.o 
+
+C_DEPS += \
+./Core/Src/crc.d \
+./Core/Src/dma.d \
+./Core/Src/dma2d.d \
+./Core/Src/fmc.d \
+./Core/Src/freertos.d \
+./Core/Src/gpio.d \
+./Core/Src/ltdc.d \
+./Core/Src/main.d \
+./Core/Src/rng.d \
+./Core/Src/sdio.d \
+./Core/Src/stm32f4xx_hal_msp.d \
+./Core/Src/stm32f4xx_it.d \
+./Core/Src/syscalls.d \
+./Core/Src/sysmem.d \
+./Core/Src/system_stm32f4xx.d \
+./Core/Src/usart.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+Core/Src/%.o Core/Src/%.su: ../Core/Src/%.c Core/Src/subdir.mk
+	arm-none-eabi-gcc "$<" -mcpu=cortex-m4 -std=gnu11 -g3 -DDEBUG -DUSE_HAL_DRIVER -DSTM32F429xx -c -I../Core/Inc -I../Drivers/STM32F4xx_HAL_Driver/Inc -I../Drivers/STM32F4xx_HAL_Driver/Inc/Legacy -I../Middlewares/Third_Party/FreeRTOS/Source/include -I../Middlewares/Third_Party/FreeRTOS/Source/CMSIS_RTOS_V2 -I../Middlewares/Third_Party/FreeRTOS/Source/portable/GCC/ARM_CM4F -I../Drivers/CMSIS/Device/ST/STM32F4xx/Include -I../Drivers/CMSIS/Include -I../FATFS/Target -I../FATFS/App -I../Middlewares/Third_Party/FatFs/src -I../LWIP/App -I../LWIP/Target -I../Middlewares/Third_Party/LwIP/src/include -I../Middlewares/Third_Party/LwIP/system -I../Middlewares/Third_Party/LwIP/src/include/netif/ppp -I../Middlewares/Third_Party/LwIP/src/include/lwip -I../Middlewares/Third_Party/LwIP/src/include/lwip/apps -I../Middlewares/Third_Party/LwIP/src/include/lwip/priv -I../Middlewares/Third_Party/LwIP/src/include/lwip/prot -I../Middlewares/Third_Party/LwIP/src/include/netif -I../Middlewares/Third_Party/LwIP/src/include/compat/posix -I../Middlewares/Third_Party/LwIP/src/include/compat/posix/arpa -I../Middlewares/Third_Party/LwIP/src/include/compat/posix/net -I../Middlewares/Third_Party/LwIP/src/include/compat/posix/sys -I../Middlewares/Third_Party/LwIP/src/include/compat/stdc -I../Middlewares/Third_Party/LwIP/system/arch -O0 -ffunction-sections -fdata-sections -Wall -fstack-usage -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@"  -mfpu=fpv4-sp-d16 -mfloat-abi=hard -mthumb -o "$@"
+
+clean: clean-Core-2f-Src
+
+clean-Core-2f-Src:
+	-$(RM) ./Core/Src/crc.d ./Core/Src/crc.o ./Core/Src/crc.su ./Core/Src/dma.d ./Core/Src/dma.o ./Core/Src/dma.su ./Core/Src/dma2d.d ./Core/Src/dma2d.o ./Core/Src/dma2d.su ./Core/Src/fmc.d ./Core/Src/fmc.o ./Core/Src/fmc.su ./Core/Src/freertos.d ./Core/Src/freertos.o ./Core/Src/freertos.su ./Core/Src/gpio.d ./Core/Src/gpio.o ./Core/Src/gpio.su ./Core/Src/ltdc.d ./Core/Src/ltdc.o ./Core/Src/ltdc.su ./Core/Src/main.d ./Core/Src/main.o ./Core/Src/main.su ./Core/Src/rng.d ./Core/Src/rng.o ./Core/Src/rng.su ./Core/Src/sdio.d ./Core/Src/sdio.o ./Core/Src/sdio.su ./Core/Src/stm32f4xx_hal_msp.d ./Core/Src/stm32f4xx_hal_msp.o ./Core/Src/stm32f4xx_hal_msp.su ./Core/Src/stm32f4xx_it.d ./Core/Src/stm32f4xx_it.o ./Core/Src/stm32f4xx_it.su ./Core/Src/syscalls.d ./Core/Src/syscalls.o ./Core/Src/syscalls.su ./Core/Src/sysmem.d ./Core/Src/sysmem.o ./Core/Src/sysmem.su ./Core/Src/system_stm32f4xx.d ./Core/Src/system_stm32f4xx.o ./Core/Src/system_stm32f4xx.su ./Core/Src/usart.d ./Core/Src/usart.o ./Core/Src/usart.su
+
+.PHONY: clean-Core-2f-Src
+
